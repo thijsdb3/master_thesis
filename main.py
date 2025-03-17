241d0c1bb335610a6478d166771bae48bf13a45a
thijsdb3 <69469687+thijsdb3@users.noreply.github.com>
2025-03-17 14:41:58 +0100

    I had an import error resolved now

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import utils
-from utils import export_hashmap_to_excel
+from hashmap import export_hashmap_to_excel
+from hashmap import create_test_hashmap
 
 tourismflanders = pd.read_excel("Thesis_Data_Preprocessed.xlsx", sheet_name="TourismFlanders", engine="openpyxl")
 combinedAirbnb = pd.read_excel("Thesis_Data_Preprocessed.xlsx", sheet_name="CombinedListingsInsideAirBNB",
@@ -8,7 +9,7 @@ combinedAirbnb = pd.read_excel("Thesis_Data_Preprocessed.xlsx", sheet_name="Comb
 
 #TODO check if it can't be all done in excel
 [tourismflanders,combinedAirbnb] = utils.preprocess_further(tourismflanders,combinedAirbnb)
-hashmap = utils.create_test_hashmap(combinedAirbnb,tourismflanders)
+hashmap = create_test_hashmap(combinedAirbnb,tourismflanders)
 export_hashmap_to_excel(hashmap)
 print(hashmap)
 
